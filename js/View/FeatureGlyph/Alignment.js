6dc6627b9d68063dd1f13e97d2fcb978a627e40a
Brigitte Hofmeister <bhofmei@gmail.com>
2016-03-07 11:45:18 -0500

    updated colors

--- js/View/FeatureGlyph/Alignment.js
+++ js/View/FeatureGlyph/Alignment.js
@@ -29,41 +29,38 @@ return declare( [BoxGlyph,MismatchesMixin], {
             //maxFeatureScreenDensity: 400
             style: {
                 color: function( feature, path, glyph, track ) {
-                    //console.log(track.config.useMammal);
                     var strand = feature.get('strand');
                     var seqLen = feature.get('seq_length');
                     if(Math.abs(strand) != 1 && strand != '+' && strand != '-')
                         return glyph.getStyle( feature, '_color_other' );
                     else if(seqLen==21)
-                        return glyph.getStyle(feature,'_color_21');
+                        return strand == 1 || strand == '+' ? glyph.getStyle(feature,'_color_21') : glyph.getStyle(feature,'_color_21_rev');
                      else if(seqLen==22)
-                        return glyph.getStyle(feature,'_color_22');
+                        return strand == 1 || strand == '+' ? glyph.getStyle(feature,'_color_22') : glyph.getStyle(feature,'_color_22_rev');
                      else if(seqLen==23)
-                        return glyph.getStyle(feature,'_color_23');
+                        return strand == 1 || strand == '+' ? glyph.getStyle(feature,'_color_23') : glyph.getStyle(feature,'_color_23_rev');
                      else if(seqLen==24)
-                        return glyph.getStyle(feature,'_color_24');
-                     else if(seqLen > 25 && seqLen < 32 && track.config.useMammal)
-                        return glyph.getStyle(feature,'_color_pi');
+                        return strand == 1 || strand == '+' ? glyph.getStyle(feature,'_color_24') : glyph.getStyle(feature,'_color_24_rev');
+                     else if(seqLen > 24 && seqLen < 32 && track.config.useMammal)
+                        return strand == 1 || strand == '+' ? glyph.getStyle(feature,'_color_pi') : glyph.getStyle(feature,'_color_pi_rev');
                     else
-                        return glyph.getStyle( feature, '_color_other' );
+                        return strand == 1 || strand == '+' ? glyph.getStyle(feature,'_color_other') : glyph.getStyle(feature,'_color_other_rev');
                 },
-                /* choose colors based on length */
-                _color_21: 'yellow',
-                _color_22: 'orange',
-                _color_23: 'green',
-                _color_24: 'purple',
-                _color_pi: 'blue',
-                _color_other: 'gray',
-                _color_border: 'black',
-                borderColor: function(feature,path,glyph,track){
-                    var strand = feature.get('strand');
-                    if(Math.abs(strand) != 1 && strand != '+' && strand != '-')
-                        return 'red';
-                    return strand == 1 || strand == "+" ? glyph.getStyle(feature,'_color_border') : null;
-                },
-                borderWidth: 1,
-
-                strandArrow: false,
+                /* choose colors based on length and strand */
+                _color_21: '#8BC240', // green
+                _color_21_rev: 'rgba(139,194,64,0.5)',
+                _color_22: '#3E98AF', // teal
+                _color_22_rev:'rgba(62,152,175,0.5)',
+                _color_23: '#A55EA4', // purple
+                _color_23_rev: 'rgba(165,94,164,0.5)',
+                _color_24: '#A94544', // red
+                _color_24_rev: 'rgba(169,69,68,0.5)',
+                _color_pi: '#F37A22', // orange
+                _color_pi_rev: 'rgba(243,122,24,0.5)',
+                _color_other: '#FDCB0A', // yellow
+                _color_other_rev: 'rgba(253,203,10,0.5)',
+                borderColor: null,
+                strandArrow: true,
 
                 height: 7,
                 marginBottom: 1,
@@ -76,18 +73,18 @@ return declare( [BoxGlyph,MismatchesMixin], {
 
     renderFeature: function( context, fRect ) {
 
-    this.inherited( arguments );
+        this.inherited( arguments );
 
-    // draw some mismatches if the feature is more than 3px wide:
-    //     draw everything if zoomed in past 0.2 px/bp, otherwise
-    //     draw only skips and deletions (the mismatches that
-    //     might be large enough to see)
-    if( fRect.w > 2 ) {
-    if( fRect.viewInfo.scale > 0.2 )
-    this._drawMismatches( context, fRect, this._getMismatches( fRect.f ) );
-    else
-    this._drawMismatches( context, fRect, this._getSkipsAndDeletions( fRect.f ));
-    }
+        // draw some mismatches if the feature is more than 3px wide:
+        //     draw everything if zoomed in past 0.2 px/bp, otherwise
+        //     draw only skips and deletions (the mismatches that
+        //     might be large enough to see)
+        if( fRect.w > 2 ) {
+            if( fRect.viewInfo.scale > 0.2 )
+                this._drawMismatches( context, fRect, this._getMismatches( fRect.f ) );
+            else
+                this._drawMismatches( context, fRect, this._getSkipsAndDeletions( fRect.f ));
+        }
     },
 
     // draw both gaps and mismatches

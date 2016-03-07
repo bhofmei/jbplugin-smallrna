ffababa5802f1ac1f46f1682bafa8f525fc60534
Brigitte Hofmeister <bhofmei@gmail.com>
2016-03-07 08:38:54 -0500

    necessary files for plugin

---
+++ js/main.js
@@ -0,0 +1,49 @@
+require({cache:{
+'JBrowse/Plugin':function(){
+define("JBrowse/Plugin", [
+           'dojo/_base/declare',
+           'JBrowse/Component'
+       ],
+       function( declare, Component ) {
+return declare( Component,
+{
+    constructor: function( args ) {
+        this.name = args.name;
+        this.cssLoaded = args.cssLoaded;
+        this._finalizeConfig( args.config );
+    },
+
+    _defaultConfig: function() {
+        return {
+            baseUrl: '/plugins/'+this.name
+        };
+    }
+});
+});
+}}});
+define('SmallRNAPlugin/main',[ 
+      'dojo/_base/declare',
+    'dojo/_base/array',
+    'dojo/_base/lang',
+    'SmallRNAPlugin/View/Track/smAlignments',
+      'JBrowse/Plugin'
+       ],
+       function(
+           declare,
+            array,
+            lang,
+            Alignments,
+           JBrowsePlugin
+       ){
+ 
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+        
+        var baseUrl = this._defaultConfig().baseUrl;
+        var thisB = this;
+        var browser = this.browser;
+        
+    } // end constructor
+});
+});

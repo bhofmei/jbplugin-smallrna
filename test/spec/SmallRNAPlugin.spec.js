2c24d80bcc632acb5dbf4eb703bc2b7df3de090d
Brigitte Hofmeister <bhofmei@gmail.com>
2017-04-13 13:55:21 -0400

    test data

---
+++ test/spec/SmallRNAPlugin.spec.js
@@ -0,0 +1,21 @@
+require([
+    'dojo/_base/declare',
+    'dojo/_base/array',
+    'JBrowse/Browser',
+    'SmallRNAPlugin/View/Track/smAlignments'
+    ], function( 
+        declare, 
+        array,
+        Browser,
+        smAlignments
+    ) {
+    
+    describe( 'Init test', function() {
+        var test = true;
+        it('test', function() {
+            expect(test).toBe(true);
+        });
+    });
+
+});
+

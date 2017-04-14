80b504ae87f6260aadf64854018878f913b5ac56
Brigitte Hofmeister <bhofmei@gmail.com>
2017-04-14 08:42:02 -0400

    adde some initialization test functions

--- test/spec/SmallRNAPlugin.spec.js
+++ test/spec/SmallRNAPlugin.spec.js
@@ -10,12 +10,45 @@ require([
         smAlignments
     ) {
     
-    describe( 'Init test', function() {
+    describe( 'Initial test', function() {
         var test = true;
-        it('test', function() {
+        it('jasmini is working', function() {
             expect(test).toBe(true);
         });
-    });
+    }); // end initial test
+    
+     describe( 'Track', function() {
+        var track = new smAlignments({
+            browser: new Browser({unitTestMode: true}),
+            config: {
+                urlTemplate: "../data/wt-smrna_chr5_short.bam",
+                label: "testtrack"
+            }
+        });
+        it('constructed', function() {
+            expect(track).toBeTruthy();
+        });
+        it('isAnimal is false', function(){
+            expect(track.config.isAnimal).toBe(false); 
+        });
+    }); // end track
+    
+    describe( 'Track with histograms', function() {
+        var track = new smAlignments({
+            browser: new Browser({unitTestMode: true}),
+            config: {
+                urlTemplate: "../data/wt-smrna_chr5_short.bam",
+                label: "testtrack",
+                histograms: {
+                    urlTemplate: "../data/wt-smrna_chr5_short.bw"
+                }
+            }
+        });
+        it('constructed', function() {
+            expect(track).toBeTruthy();
+        });
+    }); // end describe track with histograms
+    
 
 });
 

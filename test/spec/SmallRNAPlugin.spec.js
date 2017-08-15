31b264926052786c5934070df43108b9fe6fdf09
Brigitte Hofmeister <bhofmei@gmail.com>
2017-08-15 12:37:04 -0400

    updated for travis ci testing

--- test/spec/SmallRNAPlugin.spec.js
+++ test/spec/SmallRNAPlugin.spec.js
@@ -3,48 +3,68 @@ require([
     'dojo/_base/array',
     'JBrowse/Browser',
     'JBrowse/Store/SeqFeature/BAM',
-    //'JBrowse/Store/SeqFeature/BAM/LazyFeature',
     'JBrowse/Model/SimpleFeature',
     'SmallRNAPlugin/View/FeatureGlyph/smAlignment',
     'SmallRNAPlugin/View/Track/smAlignments',
+    'SmallRNAPlugin/View/Track/smHTMLAlignments',
     'JBrowse/View/FeatureGlyph/Box'
-    ], function( 
-        declare, 
-        array,
-        Browser,
-        bamStore,
-        bamFeature,
-        smAlignment,
-        smAlignments,
-        BoxGlyph
-    ) {
-    
-    describe( 'Initial test', function() {
+    ], function (
+    declare,
+    array,
+    Browser,
+    bamStore,
+    bamFeature,
+    smAlignment,
+    smAlignments,
+    smHTMLAlignments,
+    BoxGlyph
+) {
+
+    describe('Initial test', function () {
         var test = true;
-        it('jasmine is working', function() {
+        it('jasmine is working', function () {
             expect(test).toBe(true);
         });
     }); // end initial test
-    
-     describe( 'Track', function() {
+
+    describe('Track', function () {
         var track = new smAlignments({
-            browser: new Browser({unitTestMode: true}),
+            browser: new Browser({
+                unitTestMode: true
+            }),
             config: {
                 urlTemplate: "../data/test_smrna_short_adjusted.bam",
                 label: "testtrack"
             }
         });
-        it('constructed', function() {
+        it('constructed', function () {
             expect(track).toBeTruthy();
         });
-        it('isAnimal is false', function(){
-            expect(track.config.isAnimal).toBe(false); 
+        it('isAnimal is false', function () {
+            expect(track.config.isAnimal).toBe(false);
         });
     }); // end track
-    
-    describe( 'Track with histograms', function() {
+
+    describe('HTML Track', function () {
+        var track = new smHTMLAlignments({
+            browser: new Browser({
+                unitTestMode: true
+            }),
+            config: {
+                urlTemplate: "../data/test_smrna_short_adjusted.bam",
+                label: "testtrack-html"
+            }
+        });
+        it('constructed', function () {
+            expect(track).toBeTruthy();
+        });
+    }); // end html track
+
+    describe('Track with histograms', function () {
         var track = new smAlignments({
-            browser: new Browser({unitTestMode: true}),
+            browser: new Browser({
+                unitTestMode: true
+            }),
             config: {
                 urlTemplate: "../data/test_smrna_short_adjusted.bam",
                 label: "testtrack",
@@ -53,53 +73,91 @@ require([
                 }
             }
         });
-        it('constructed', function() {
+        it('constructed', function () {
             expect(track).toBeTruthy();
         });
     }); // end describe track with histograms
-    
-    describe('Test features and filters', function(){
+
+    describe('Test features and filters', function () {
         var store = new bamStore({
-            browser: new Browser({unitTestMode: true}),
+            browser: new Browser({
+                unitTestMode: true
+            }),
             urlTemplate: '../data/test_smrna_short_adjusted.bam',
-            refSeq: { name: 'Chr5', start: 0, end: 50001 }
+            refSeq: {
+                name: 'Chr5',
+                start: 0,
+                end: 50001
+            }
         });
-        it('constructed', function() {
+        it('constructed', function () {
             expect(store).toBeTruthy();
         });
         var features = [];
-        beforeEach(function(done){
-            store.getFeatures({start:2500, end: 5000}, function(feature){
+        beforeEach(function (done) {
+            store.getFeatures({
+                start: 2500,
+                end: 5000
+            }, function (feature) {
                 features.push(feature);
-            }, function(){
+            }, function () {
                 done();
-            }, function(error){
+            }, function (error) {
                 console.error(error);
                 done();
             });
         });
-        afterEach(function(){
+        afterEach(function () {
             features = [];
         });
-        it('all features', function(){
+        it('all features', function () {
             expect(features.length).toEqual(169);
         });
-        it('test filters', function(){
-            var len24 = array.filter(features, function(f) { return f.get('seq_length')===24; });
-            var len22 = array.filter(features, function(f) { return f.get('seq_length')===22; });
-            var plusStrand = array.filter(features, function(f) { return (f.get('strand')==="+" || f.get('strand')===1); });
-            var seqTest = array.filter(features, function(f) { return f.get('seq')==="TTGTGGTTGTTCTTAGGCTTCAGT"; });
-            var multitest = array.filter(features, function(f) { return f.get('nh')>1; });
-            var qualtest = array.filter(features, function(f) { return (f.get('score')>10 && f.get('score')<=30 ); });
+        describe('test filters', function () {
+            it('len 24 filter', function () {
+                var len24 = array.filter(features, function (f) {
+                    return f.get('seq_length') === 24;
+                });
+                expect(len24.length).toBe(105);
+            });
 
-            expect(len24.length).toBe(105);
-            expect(len22.length).toBe(3);
-            expect(plusStrand.length).toBe(36);
-            expect(seqTest.length).toBe(5);
-            expect(multitest.length).toBe(51);
-            expect(qualtest.length).toBe(71);
+            it('len 22 filter', function () {
+                var len22 = array.filter(features, function (f) {
+                    return f.get('seq_length') === 22;
+                });
+                expect(len22.length).toBe(3);
+            });
+
+            it('plus strand filter', function () {
+                var plusStrand = array.filter(features, function (f) {
+                    return (f.get('strand') === "+" || f.get('strand') === 1);
+                });
+                expect(plusStrand.length).toBe(36);
+            });
+
+            it('sequence filter', function () {
+                var seqTest = array.filter(features, function (f) {
+                    return f.get('seq') === "TTGTGGTTGTTCTTAGGCTTCAGT";
+                });
+                expect(seqTest.length).toBe(5);
+            });
+
+            it('multimapped filter', function () {
+                var multitest = array.filter(features, function (f) {
+                    return f.get('nh') > 1;
+                });
+                expect(multitest.length).toBe(51);
+            });
+
+            it('quality filter', function () {
+
+
+                var qualtest = array.filter(features, function (f) {
+                    return (f.get('score') > 10 && f.get('score') <= 30);
+                });
+                expect(qualtest.length).toBe(71);
+            });
         });
     });
 
 });
-
